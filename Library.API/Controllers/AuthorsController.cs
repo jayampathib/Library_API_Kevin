b4409467e3baf32a9a1026f3a5d42de1bcd1362f
jayampathib <wmjb.weerasinghe@gmail.com>
2019-11-07 19:17:36 +0800

    Add project files.

---
+++ Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            Author author = new Author
+            {
+                Id = new Guid("25320c5e-f58a-4b1f-b63a-8ee07a840bdf"),
+                FirstName = "Stephen",
+                LastName = "King",
+                Genre = "Horror",
+                DateOfBirth = new DateTimeOffset(new DateTime(1947, 9, 21))
+            };
+            return Ok(author);
+        }
+    }
+}
